b31bf276188ba338af8c8f1b4c1645770917cc97
Sehun Oh <ingerest1@naver.com>
2025-03-31 14:46:51 +0900

    App1 TC

--- Project31/main.cpp
+++ Project31/main.cpp
@@ -1,5 +1,116 @@
 #include "gmock/gmock.h"
 
+#include <sstream>
+#include <iostream>
+
+using namespace testing;
+
+class MockStockBrocker : public IStockBrocker {
+public:
+    MOCK_METHOD(void, login, (const std::string&, const std::string&), (override));
+    MOCK_METHOD(void, buy, (const std::string&, int, int), (override));
+    MOCK_METHOD(void, sell, (const std::string&, int, int), (override));
+    MOCK_METHOD(int, getPrice, (const std::string&), (override));
+};
+
+TEST(APP1, Login)
+{
+    MockStockBrocker mockBrocker;
+    AutoTrader trader(&mockBrocker);
+
+    std::string testId = "user123";
+    std::string testPass = "pass123";
+
+    EXPECT_CALL(mockBrocker, login(testId, testPass))
+        .Times(1)
+        .WillOnce([&](const std::string& id, const std::string& pass) {
+                std::cout << id << " login success\n";
+            });
+
+    std::stringstream buffer;
+    std::streambuf* originalCout = std::cout.rdbuf();
+    std::cout.rdbuf(buffer.rdbuf());
+
+    trader.login(testId, testPass);
+
+    std::cout.rdbuf(originalCout);
+
+    std::string output = buffer.str();
+    EXPECT_NE(output.find("user123 login success"), std::string::npos);
+}
+
+TEST(APP1, Buy)
+{
+    MockStockBrocker mockBrocker;
+    AutoTrader trader(&mockBrocker);
+
+    std::string stockCode = "TESLA";
+    int count = 100;
+    int price = 380000;
+
+    EXPECT_CALL(mockBrocker, buy(stockCode, count, price))
+        .Times(1)
+        .WillOnce([&](const std::string& id, int count, int price) {
+             std::cout << stockCode << " : Buy stock ( " << price << " * " << count << ")\n";
+            });
+
+    std::stringstream buffer;
+    std::streambuf* originalCout = std::cout.rdbuf();
+    std::cout.rdbuf(buffer.rdbuf());
+
+    trader.buy;
+
+    std::cout.rdbuf(originalCout);
+
+    std::string output = buffer.str();
+    EXPECT_NE(output.find("TESLA : Buy stock ( 380000 * 100 )"), std::string::npos);
+}
+
+TEST(APP1, Sell)
+{
+    MockStockBrocker mockBrocker;
+    AutoTrader trader(&mockBrocker);
+
+    std::string stockCode = "TESLA";
+    int count = 100;
+    int price = 380000;
+
+    EXPECT_CALL(mockBrocker, buy(stockCode, count, price))
+        .Times(1)
+        .WillOnce([&](const std::string& id, int count, int price) {
+        std::cout << stockCode << " : Sell stock ( " << price << " * " << count << ")\n";
+            });
+
+    std::stringstream buffer;
+    std::streambuf* originalCout = std::cout.rdbuf();
+    std::cout.rdbuf(buffer.rdbuf());
+
+    trader.buy;
+
+    std::cout.rdbuf(originalCout);
+
+    std::string output = buffer.str();
+    EXPECT_NE(output.find("TESLA : Sell stock ( 380000 * 100 )"), std::string::npos);
+}
+
+TEST(APP1, getPrice)
+{
+    
+    MockStockBrocker mockBrocker;
+    AutoTrader trader(&mockBrocker);
+
+    std::string stockCode = "TESLA";
+    int expectedPrice = 5432;
+
+    EXPECT_CALL(mockBrocker, getPrice(stockCode))
+        .Times(1)
+        .WillOnce(testing::Return(expectedPrice));
+
+    int price = trader.getPrice(stockCode);
+
+    EXPECT_EQ(price, expectedPrice);
+}
+
 int main()
 {
 	::testing::InitGoogleMock();
